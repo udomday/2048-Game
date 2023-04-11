2bbf475d7809e9d068c2869910f20b713bbe9266
udomday <SunHay@mail.ru>
2023-04-11 22:28:15 +0300

    not working version

--- sctripts/gameController.js
+++ sctripts/gameController.js
@@ -1,7 +1,8 @@
+const tileConteiner = document.querySelector(".tile-conteiner")
+
 function gameStart(array){
     newNumberOnBoard(array)
     newNumberOnBoard(array)
-    printArrayOnBoard(array)
 }
 
 function newNumberOnBoard(array){
@@ -11,15 +12,21 @@ function newNumberOnBoard(array){
     let x = getIndexFromNumber(random_num)[0]
     let y = getIndexFromNumber(random_num)[1]
     array = insert_2_or_4(array, x, y)
+    tileConteiner.insertAdjacentHTML('afterend', `<div class="tile tile-${array[x][y].num} tile-position-${x+1}-${y+1}"><div class="tile-inner">${array[x][y].num}</div></div>`)
     console.log(`Мы заполнили ячейку под номером ${random_num}`)
     printArray(array)
 }
 
 function printArray(array){
+    let printString = ''
     console.log('---------------')
-    array.forEach(el => {
-        console.log(...el)
+    array.forEach(row => {
+        row.forEach(el => {
+            printString += `${el != 0 ? el.num : 0}`
+        })
+        printString += '\n'
     });
+    console.log(printString)
     console.log('---------------')
 }
 
@@ -52,9 +59,9 @@ function getIndexFromNumber(num){
 
 function insert_2_or_4(array, x, y){
     if(Math.random() <= 0.75){
-        array[x][y] = 2
+        array[x][y] = {num: 2, x: x, y: y, position:`tile-position-${x+1}-${y+1}`}
     } else{
-        array[x][y] = 4
+        array[x][y] = {num: 4, x: x, y: y, position:`tile-position-${x+1}-${y+1}`}
     }
     return array
 }
@@ -81,30 +88,53 @@ function isZeroInMas(array){
     return false
 }
 
+function animationMove(array){
+    for(let i = 0; i < array.length; i++){
+        array[i] = array[i].map((elem, count = 0)=>{
+            if(elem != 0){
+                let tile = document.querySelector(`.tile-position-${elem.x+1}-${elem.y+1}`)
+                tile.classList.remove(`tile-position-${elem.x+1}-${elem.y+1}`)
+                tile.classList.add(`tile-position-${i+1}-${count+1}`)
+                elem.x = i
+                elem.y = count
+                elem.position = `tile-position-${i+1}-${count+1}`
+            }
+            count++
+            return elem
+        })
+    }
+}
+
 function moveLeft(array, canMoveArray){
     let delta = 0
-    array = array.map(row => {
-        while(row.includes(0)){
-            row = row.filter(function(elem) {
+    for(let i = 0; i < array.length; i++){
+        while(array[i].includes(0)){
+            array[i] = array[i].filter(function(elem) {
                 return elem != 0;
             })
         }
-        while(row.length != 4){
-            row.push(0)
+        while(array[i].length != 4){
+            array[i].push(0)
         }
-        return row
-    })
+    }
     for(let i = 0; i < 4; i++){
         for(let j = 0; j < 3; j++){
-            if(array[i][j] === array[i][j+1] && array[i][j] != 0){
-                array[i][j] = array[i][j]*2
-                delta = array[i][j]
+            if(array[i][j].num === array[i][j+1].num && array[i][j] != 0){
+                document.querySelector(`.tile-${array[i][j].num} .tile-position-${array[i][j].x+1}-${array[i][j].y+1}`).classList.add(`deleteTile`)
+                array[i][j].num = array[i][j].num*2
+                tileConteiner.insertAdjacentHTML('afterend', `<div class="tile tile-${array[i][j].num} tile-position-${array[i][j].x+1}-${array[i][j].y+1}"><div class="tile-inner">${array[i][j].num}</div></div>`)
+                delta = array[i][j].num
+                let tile = document.querySelector(`.tile-position-${array[i][j+1].x+1}-${array[i][j+1].y+1}`)
+                tile.classList.remove(`tile-position-${array[i][j+1].x+1}-${array[i][j+1].y+1}`)
+                tile.classList.add(`tile-position-${array[i][j].x+1}-${array[i][j].y+1}`)
+                tile.classList.add('deleteTile')
                 array[i].splice(j+1, 1)
                 array[i].push(0)
             }
-       }
+        }
     }
     if(isZeroInMas(array) && !arraysEqual(array, canMoveArray)){newNumberOnBoard(array)}
+    animationMove(array)
     return [array, delta]
 }
 
@@ -123,15 +153,27 @@ function moveRight(array, canMoveArray){
     })
     for(let i = 0; i < 4; i++){
         for(let j = 3; j > 0; j--){
-            if(array[i][j] === array[i][j-1] && array[i][j] != 0){
-                array[i][j] = array[i][j]*2
-                delta = array[i][j]
+            if(array[i][j].num === array[i][j-1].num && array[i][j] != 0){
+                // array[i][j] = array[i][j]*2
+                // delta = array[i][j]
+                // array[i].splice(j-1, 1)
+                // array[i].unshift(0)
+                /////
+                document.querySelector(`.tile-position-${array[i][j].x+1}-${array[i][j].y+1}`).classList.add(`deleteTile`)
+                array[i][j].num = array[i][j].num*2
+                tileConteiner.insertAdjacentHTML('afterend', `<div class="tile tile-${array[i][j].num} tile-position-${array[i][j].x+1}-${array[i][j].y+1}"><div class="tile-inner">${array[i][j].num}</div></div>`)
+                delta = array[i][j].num
+                let tile = document.querySelector(`.tile-position-${array[i][j-1].x+1}-${array[i][j-1].y+1}`)
+                tile.classList.remove(`tile-position-${array[i][j-1].x+1}-${array[i][j-1].y+1}`)
+                tile.classList.add(`tile-position-${array[i][j].x+1}-${array[i][j].y+1}`)
+                tile.classList.add('deleteTile')
                 array[i].splice(j-1, 1)
                 array[i].unshift(0)
             }
-       }
+        }
     }
     if(isZeroInMas(array) && !arraysEqual(array, canMoveArray)){newNumberOnBoard(array)}
+    animationMove(array)
     return [array, delta]
 }
 
@@ -196,17 +238,17 @@ function canMove(array){
         for(let j = 0; j < 4; j++){
             if(i == 3){
                if(j != 3){
-                if(array[i][j]==array[i][j+1]){
+                if(array[i][j].num==array[i][j+1].num){
                     return true
                 }
                } else {
                 return false
                }
             } else{
-                if(j == 3 && array[i][j]==array[i+1][j]){
+                if(j == 3 && array[i][j].num==array[i+1][j].num){
                     return true
                 }
-                else if(array[i][j]==array[i][j+1] || array[i][j]==array[i+1][j]){
+                else if(array[i][j].num==array[i][j+1].num || array[i][j].num==array[i+1][j].num){
                     return true
                 }
             }
@@ -223,7 +265,7 @@ function arraysEqual(array1, array2) {
     for (let i = 0; i < array1.length; i++) {
         if(array1[i].length != array2[i].length) return false
         for(let j = 0; j < array1[i].length; j++){
-            if (array1[i][j] !== array2[i][j]) return false;
+            if (array1[i][j].num !== array2[i][j].num) return false;
         }
     }
     return true;
--- sctripts/script.js
+++ sctripts/script.js
@@ -8,63 +8,57 @@ let tileArray = [
     [0, 0, 0, 0]
 ];
 
-let testArray = [
-    [0, 0, 2, 0], 
-    [0, 0, 0, 0], 
-    [0, 0, 0, 0], 
-    [0, 0, 0, 0]
-]
-
-let testArray2 = [
-    [0, 0, 0, 0], 
-    [0, 0, 0, 0], 
-    [0, 0, 0, 0], 
-    [4, 0, 0, 0]
-]
-
-console.log('TEST ', arraysEqual(testArray, testArray2))
-
 let score = 0;
 
 const scoreBar = document.querySelector('.score-bar')
 
 gameStart(tileArray)
 
+function deleteTiles(){
+    let deleteTile = document.querySelectorAll('.deleteTile')
+        deleteTile.forEach(el => {
+            el.remove()
+        })
+}
+
 document.addEventListener("keydown", function(event){
     if(isZeroInMas(tileArray) || canMove(tileArray)){
         var canMoveArray = structuredClone(tileArray)
         switch(event.code){
-            
             case 'ArrowLeft':
                 let arrayMoveLeft = moveLeft(tileArray, canMoveArray)
                 tileArray = arrayMoveLeft[0]
                 score += arrayMoveLeft[1]
                 scoreBar.innerHTML = `Счет: ${score}`
-                printArrayOnBoard(tileArray)
+                //if(isZeroInMas(array) && !arraysEqual(array, canMoveArray)){newNumberOnBoard(array)}
+                //printArrayOnBoard(tileArray)
+                setTimeout(deleteTiles, 310);
                 break
             case 'ArrowRight':
                 let arrayMoveRigth = moveRight(tileArray, canMoveArray)
                 tileArray = arrayMoveRigth[0]
                 score += arrayMoveRigth[1]
                 scoreBar.innerHTML = `Счет: ${score}`
-                printArrayOnBoard(tileArray)
+                //printArrayOnBoard(tileArray)
+                setTimeout(deleteTiles, 310);
                 break
             case 'ArrowUp':
                 let arrayMoveUp = moveUp(tileArray, canMoveArray)
                 tileArray = arrayMoveUp[0]
                 score += arrayMoveUp[1]
                 scoreBar.innerHTML = `Счет: ${score}`
-                printArrayOnBoard(tileArray)
+                //printArrayOnBoard(tileArray)
                 break
             case 'ArrowDown':
                 let arrayMoveDown = moveDown(tileArray, canMoveArray)
                 tileArray = arrayMoveDown[0]
                 score += arrayMoveDown[1]
                 scoreBar.innerHTML = `Счет: ${score}`
-                printArrayOnBoard(tileArray)
+                //printArrayOnBoard(tileArray)
                 break    
 
         }
+        
     }else{
         console.log("Game Over")
     }
