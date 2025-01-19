be0cf3f9d67d8d17ff9cbe03142ede5c88696eb3
udomday <sunhay@mail.ru>
2025-01-19 23:10:05 +0300

    Test redirect

---
+++ 2048/scripts/script.js
@@ -0,0 +1,195 @@
+import { Grid } from "./grid.js";
+import { Tile } from "./tile.js";
+
+const gameBoard = document.getElementById('game-board');
+const scoreBar = document.getElementById('score');
+const grid = new Grid(gameBoard);
+
+document.getElementById('restart-game').addEventListener('click', () => restartGame());
+
+function gameStart(){
+    grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
+    grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
+    setUpInputOnce();
+}
+
+gameStart();
+
+function setUpInputOnce(){
+    window.addEventListener('keydown', handleInput, {once: true});
+}
+
+async function handleInput(event) {
+    switch(event.key) {
+        case 'ArrowUp':
+            if(!canMoveUp()){
+                setUpInputOnce();
+                return;
+            }
+            await moveUp();
+            break;
+        
+        case 'ArrowDown':
+            if(!canMoveDown()){
+                setUpInputOnce();
+                return;
+            }
+            await moveDown();
+            break;
+        
+        case 'ArrowLeft':
+            if(!canMoveLeft()){
+                setUpInputOnce();
+                return;
+            }
+            await moveLeft();
+            break;
+        
+        case 'ArrowRight':
+            if(!canMoveRight()){
+                setUpInputOnce();
+                return;
+            }
+            await moveRight();
+        break;
+        
+        default:
+            setUpInputOnce();
+            return;
+    }
+
+    const newTile = new Tile(gameBoard);
+    grid.getRandomEmptyCell().linkTile(newTile);
+
+    if(!canMoveUp() && !canMoveDown() && !canMoveLeft() && !canMoveRight()){
+        await newTile.waitForAnimationEnd();
+        alert('Try again!');
+        restartGame();
+        return;
+    }
+
+    if(winGame()){
+        await newTile.waitForAnimationEnd();
+        alert('Поздравляем, Вы победили!');
+        restartGame();
+        return;
+    }
+
+    setUpInputOnce();
+}
+
+async function moveUp(){
+    await slideTiles(grid.cellsGroupedByColumn);
+}
+
+async function moveDown(){
+    await slideTiles(grid.cellsGroupedByReversedColumn);
+}
+
+async function moveLeft(){
+    await slideTiles(grid.cellsGroupedByRow);
+}
+
+async function moveRight(){
+    await slideTiles(grid.cellsGroupedByReversedRow);
+}
+
+async function slideTiles(groupedCells){
+    const promises = [];
+    
+    groupedCells.forEach(group => slideTilesInGroup(group, promises));
+
+    await Promise.all(promises);
+
+    grid.cells.forEach(cell => {
+        cell.hasTileForMerge() && cell.mergeTiles();
+    })
+}
+
+function winGame(){
+    let flag = false
+    grid.cells.forEach(cell => {
+        if(!!cell.linkedTile  || !!cell.linkedTileForMerge){
+            if(cell.linkedTile.value === 2048){
+                flag = true
+            };
+        }
+    });
+    return flag
+}
+
+function restartGame(){
+    grid.cells.forEach(cell => {
+        if(!!cell.linkedTile  || !!cell.linkedTileForMerge){
+            cell.removeCellsFromBoard();
+        }
+    });
+
+    gameStart();
+}
+
+function slideTilesInGroup(group, promises){
+    for(let i = 1; i<group.length; i++){
+        if(group[i].isEmpty()){
+            continue;
+        }
+        const cellWithTile = group[i];
+
+        let targetCell;
+        let j = i - 1;
+        while(j>=0 && group[j].canAccept(cellWithTile.linkedTile)){
+            targetCell = group[j];
+            j--;
+        }
+
+        if(!targetCell){
+            continue;
+        }
+
+        promises.push(cellWithTile.linkedTile.waitForTransitionEnd());
+
+        if(targetCell.isEmpty()){
+            targetCell.linkTile(cellWithTile.linkedTile);
+        } else {
+            targetCell.linkTileForMerge(cellWithTile.linkedTile);
+        }
+
+        cellWithTile.unlinkTile();
+    }
+}
+
+function canMoveUp(){
+    return canMove(grid.cellsGroupedByColumn);
+}
+
+function canMoveDown(){
+    return canMove(grid.cellsGroupedByReversedColumn);
+}
+
+function canMoveLeft(){
+    return canMove(grid.cellsGroupedByRow);
+}
+
+function canMoveRight(){
+    return canMove(grid.cellsGroupedByReversedRow);
+}
+
+function canMove(groupedCells){
+    return groupedCells.some(group => canMoveInGroup(group));
+}
+
+function canMoveInGroup(group){
+    return group.some((cell, index) => {
+
+        if(index === 0) {
+            return false;
+        }
+
+        if(cell.isEmpty()){
+            return false;
+        }
+
+        const targetCell = group[index - 1];
+        return targetCell.canAccept(cell.linkedTile);
+    });
+}
---
+++ 2048/scripts/tile.js
@@ -0,0 +1,54 @@
+const tileColor = {
+    2: `#eee4da`,
+    4: `#ede0c8`,
+    8: `#f2b179`,
+    16: `#f59563`,
+    32: `#f67c5f`,
+    64: `#f65e3b`,
+    128: `#edcf72`,
+    256: `#edcc61`,
+    512: `#edc850`,
+    1024: `#edc53f`,
+    2048: `#edc22e`
+}
+
+export class Tile {
+    constructor(gridElement){
+        this.tileElemnt = document.createElement('div');
+        this.tileElemnt.classList.add('tile');
+        this.setValue(Math.random() < 0.75 ? 2 : 4);
+        gridElement.append(this.tileElemnt);
+    }
+
+    setXY(x, y){
+        this.x = x;
+        this.y =y;
+        this.tileElemnt.style.setProperty('--x', x);
+        this.tileElemnt.style.setProperty('--y', y);
+    }
+
+    setValue(value){
+        this.value = value
+        this.tileElemnt.textContent = value;
+        const bgLightness = 100 - Math.log2(value) * 9;
+        this.tileElemnt.style.setProperty('--bg-lightness', `${tileColor[value]}`);
+        this.tileElemnt.style.setProperty('--text-lightness', `${value >= 8 ? `#f9f6f2` : `#292828`}`)
+        
+    }
+
+    removeFromDOM(){
+        this.tileElemnt.remove();
+    }
+
+    waitForTransitionEnd(){
+        return new Promise(resolve => {
+            this.tileElemnt.addEventListener('transitionend', resolve, {once: true})
+        });
+    }
+
+    waitForAnimationEnd(){
+        return new Promise(resolve => {
+            this.tileElemnt.addEventListener('animationend', resolve, {once: true})
+        });
+    }
+}
